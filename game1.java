96d3ca8bca77b4ed9ecb6ce98ee09c0bf64ebf4a
kristoff0531 <kroy66194@gmail.com>
2021-10-25 22:58:25 +0530

    first_push

---
+++ game1.java
@@ -0,0 +1,51 @@
+package com.company;
+
+import java.util.Random;
+import java.util.Scanner;
+
+class game1 {
+        private int user;
+        private final int random;
+        private int guess = 1;
+
+        public game1() {
+            Random r = new Random();
+            this.random = r.nextInt(50);
+        }
+
+        public void takeInput() {
+            Scanner se = new Scanner(System.in);
+            this.user = se.nextInt();
+        }
+
+        public void isThat() {
+            if (this.user == this.random) {
+                System.out.println("congratulations !! you have got it...");
+            }
+
+            if (this.user > this.random) {
+                System.out.println("guess a low");
+                this.setGuess();
+                this.takeInput();
+                this.isThat();
+            }
+
+            if (this.user < this.random) {
+                System.out.println("choose a bit higher");
+                this.setGuess();
+                this.takeInput();
+                this.isThat();
+            }
+
+        }
+
+        public void no_ofGuess() {
+            System.out.printf("you have taken %d number of attempts",guess);
+        }
+
+        public void setGuess() {
+            ++guess;
+        }
+    }
+
+
